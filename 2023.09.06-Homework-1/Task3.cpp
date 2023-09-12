21963810c7ff7d5dd1bad48da93bd074fe77f970
st117158 <144161699+st117158@users.noreply.github.com>
2023-09-12 19:43:43 +0300

    Lesson 1 code update

--- 2023.09.06-Homework-1/Task3.cpp
+++ 2023.09.06-Homework-1/Task3.cpp
@@ -4,7 +4,11 @@ int main(int argc, char* argv[])
 {
     int a = 0;
     std::cin >> a;
-    int b = a * a;
-    std::cout << b;
+    int b = a / 10;
+    int c = b + 1;
+    int d = b * c;
+    int e = d * 100;
+    int f = e + 25;
+    std::cout << f;
     return EXIT_SUCCESS;
 }
