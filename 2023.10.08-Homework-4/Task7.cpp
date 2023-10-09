48c1290456dcf56a7a1ddb0b58c711c2f1eff421
st117158 <144161699+st117158@users.noreply.github.com>
2023-10-09 22:54:10 +0300

    Lesson 4 code upload

--- 2023.10.08-Homework-4/Task7.cpp
+++ 2023.10.08-Homework-4/Task7.cpp
@@ -3,33 +3,33 @@
 
 int main(int argc, char**)
 {
-	int n = 0;
-	std::cin >> n;
-	int* a = (int*)malloc(sizeof(int) * n);
-	int res_pos = 0;
+    int n = 0;
+    std::cin >> n;
+    int* a = (int*)malloc(sizeof(int) * n);
+    int res_pos = 0;
 
-	for (int i = 0; i < n; ++i)
-	{
-		std::cin >> *(a + i);
-	}
-	int k = 0;
-
-	for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
-         if (*(a + i) >= 0)
-         {
-             *(a + k) = *(a + i);
-             k++;
-         }
+        std::cin >> *(a + i);
+    }
+    int k = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (*(a + i) >= 0)
+        {
+            *(a + k) = *(a + i);
+            k++;
+        }
     }
     n = k;
     a = (int*)realloc(a, sizeof(int) * n);
 
     for (int i = 0; i < n; ++i)
     {
-       std::cout << *(a + i) << " ";
+        std::cout << *(a + i) << " ";
     }
-	free(a);
+    free(a);
 
     return EXIT_SUCCESS;
 }
