c70955606c6114dbc90db639a0cc2253a7893872
st117158 <144161699+st117158@users.noreply.github.com>
2023-11-29 21:32:14 +0300

    Lesson 6 code upload

--- 2023.11.22-Homework-6/Task5.cpp
+++ 2023.11.22-Homework-6/Task5.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 
-void sort_help(int n, int i, int l)
+void sort_h(int n, int i, int l)
 {
     if(n > 0)
     {
         int k = 6 - i - l;
-        sort_help(n - 1, i, k);
+        sort_h(n - 1, i, k);
         std::cout << n << " " << i << " " << l << std::endl;
-        sort_help(n - 1, k, l);
+        sort_h(n - 1, k, l);
     }
 }
 
-void sort_h(int n)
+void sort_hanoy(int n)
 {
     int l = 0;
     if(n % 2 == 0)
@@ -23,11 +23,11 @@ void sort_h(int n)
         l = 2;
     }
     int i = 1;
-    sort_help(n, i, l);
+    sort_h(n, i, l);
     int k = 5 - l;
     for(int i = n; i > 0; i--)
     {
-        sort_help(i - 1, l, k);
+        sort_h(i - 1, l, k);
         std::swap(l, k);
     }
 }
@@ -36,7 +36,7 @@ int main(int argc, char* argv[])
 {
     int n = 0;
     std::cin >> n;
-    sort_h(n);
+    sort_hanoy(n);
     std::cout << std::endl;
 
     return EXIT_SUCCESS;
