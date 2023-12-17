c865026118dbeea1e1df7228862541853389c8d7
st117158 <144161699+st117158@users.noreply.github.com>
2023-12-17 13:23:55 +0300

    Lesson 6 code update

--- 2023.11.22-Homework-6/Task6.cpp
+++ 2023.11.22-Homework-6/Task6.cpp
@@ -1,33 +1,33 @@
 #include<iostream>
 
-void exchange_towers(int n, int from , int to)
+void exchange_towers(int n, int from, int to)
 {
-	if (n <= 0)
-	{
-		return;
-	}
+    if(n <= 0)
+    {
+        return;
+    }
 
-	int res = 6 - from - to;
+    int res = 6 - from - to;
 
-	if (n == 1)
-	{
-		std::cout << 1 << " " << from << " " << to << std::endl;
-	}
-	else
-	{
-		exchange_towers(n - 1, from, to);
+    if(n == 1)
+    {
+        std::cout << 1 << " " << from << " " << to << std::endl;
+    }
+    else
+    {
+        exchange_towers(n - 1, from, to);
         exchange_towers(n - 2, to, res);
-		std::cout << 0 << " " << from << " " << to << std::endl;
-		exchange_towers(n - 2, res, from);
-		exchange_towers(n - 1, from, to);
-	}
+        std::cout << 0 << " " << from << " " << to << std::endl;
+        exchange_towers(n - 2, res, from);
+        exchange_towers(n - 1, from, to);
+    }
 }
 
 
 int main(int argc, char* argv[])
 {
-	int n = 0;
-	std::cin >> n;
-	exchange_towers(n, 1, 3);
-	return EXIT_SUCCESS;
+    int n = 0;
+    std::cin >> n;
+    exchange_towers(n, 1, 3);
+    return EXIT_SUCCESS;
 }
